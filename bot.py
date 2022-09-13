9d8bceacb589b69e4bf48d559125f09b70145b89
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2022-09-14 01:06:37 +0400

    ffd

--- bot.py
+++ bot.py
@@ -81,7 +81,7 @@ def abouts(client, message):
 def stats(client, message):
     stat = client.send_message(
         chat_id=message.chat.id,
-        reply_to_message_id=message.chat_id,
+        reply_to_message_id=message.id,
         text="`Fetching details`",
     )
     txt = logreturn()
